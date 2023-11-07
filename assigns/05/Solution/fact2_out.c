bc91d1aa4ad245f2108219cdc64fc9e38a9772b2
Hongwei Xi <gmhwxi@gmail.com>
2023-11-06 19:30:59 -0500

    Updating: very very minorly

---
+++ assigns/05/Solution/fact2_out.c
@@ -0,0 +1,45 @@
+/* ****** ****** */
+
+#include "runtime2.h"
+
+/* ****** ****** */
+
+extern
+void*
+mymalloc(size_t n) {
+  void* p0;
+  p0 = malloc(n);
+  if (p0 != 0) return p0;
+  fprintf(stderr, "myalloc failed!!!\n");
+  exit(1);
+}
+
+/* ****** ****** */
+
+/*
+HX-2023-11-05:
+Please translate the following function
+into C code of the style given in fact_out.c
+
+fun
+fact2(n:int): int =
+let
+//
+(*
+How do you handle an inner function like
+loop in your translation?
+*)
+//
+fun
+loop(i: int, r: int): int =
+if i < n
+then loop(i+1, (i+1) * r) else r
+//
+in
+  loop(0, 1)
+end
+*/
+
+/* ****** ****** */
+
+/* end of [CS525-2023-Fall/assigns/assign05/Solution/fact2_out.c] */
---
+++ assigns/05/Solution/facti_out.c
@@ -0,0 +1,45 @@
+/* ****** ****** */
+
+#include "runtime2.h"
+
+/* ****** ****** */
+
+extern
+void*
+mymalloc(size_t n) {
+  void* p0;
+  p0 = malloc(n);
+  if (p0 != 0) return p0;
+  fprintf(stderr, "myalloc failed!!!\n");
+  exit(1);
+}
+
+/* ****** ****** */
+
+/*
+HX-2023-11-05:
+Please translate the following function
+into C code of the style given in fact_out.c
+
+fun
+fact2(n:int): int =
+int_foldleft(n, 1, lam(r, i) => r * (i+1))
+
+where 'int_foldleft' is defined as follows:
+
+fun
+int_foldleft
+(n0, r0, fopr) =
+(
+  loop(0, r0)) where
+{
+fun
+loop(i0) =
+if (i0 >= n0) then r0 else loop(i0+1, fopr(r0, i0))
+}
+
+*/
+
+/* ****** ****** */
+
+/* end of [CS525-2023-Fall/assigns/assign05/Solution/facti_out.c] */
