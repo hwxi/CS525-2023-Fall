d911c0bbfa742d6a6715ca880343e2eeb0c541f0
Hongwei Xi <gmhwxi@gmail.com>
2023-11-06 19:13:24 -0500

    Updating: very very minorly

---
+++ assigns/05/Solution/runtime2.h
@@ -0,0 +1,11 @@
+/* ****** ****** */
+#include "./../runtime.h"
+/* ****** ****** */
+
+/*
+HX: Please add whatever you need below.
+*/
+
+/* ****** ****** */
+
+/* end of [CS525-2023-Fall/assigns/assign05/Solution/runtime2.h] */
